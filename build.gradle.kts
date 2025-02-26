00073acc0061b560ff5394dec630f685da1cad85
mathiasbd <s235270@dtu.dk>
2025-02-26 13:58:15 +0100

    TEst

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val lwjglVersion = "3.3.6"
+val jomlVersion = "1.10.7"
+val lwjglNatives = "natives-windows"
+
 repositories {
     mavenCentral()
 }
@@ -12,6 +16,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
 }
 
 tasks.test {
