99ac43b8ec507437790e130d804f165cfc553311
AhmedAbikar <148544113+AhmedAbikar@users.noreply.github.com>
2025-03-05 16:56:16 +0100

    Draw the rectangle with openGL
    but there is an error that i can't find

--- src/main/java/org/example/LevelEditorScene.java
+++ src/main/java/org/example/LevelEditorScene.java
@@ -1,10 +1,16 @@
 package org.example;
 
+
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+import org.lwjgl.BufferUtils;
+import org.lwjgl.Version;
+import static org.lwjgl.opengl.GL30.*;
+import java.nio.IntBuffer;
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
 import static org.lwjgl.opengl.GL20.*;
 
 public class LevelEditorScene extends Scene {
@@ -35,6 +41,18 @@ public class LevelEditorScene extends Scene {
 
     private int vertexID, fragmentID, shaderProgram;
 
+    private float [] vertexArray={
+            //postion              //color
+            0.5f, -0.5f, 0.0f,      1.0f,0.0f,0.0f,1.0f,   //pos: bottom right, col: red,0
+            -0.5f, 0.5f, 0.0f,      0.0f,1.0f,0.0f,1.0f,   //pos: Top Left, col: green, 1
+            0.5f, 0.5f, 0.0f,       0.0f,0.0f,1.0f,1.0f,    //pos: Top Right, col: Blue, 2
+            -0.5f, 0.5f, 0.0f,      1.0f,1.0f,0.0f,1.0f,   //pos: Bottom Left, col: yellow, 3
+    };
+    private int [] elementArray={
+            2,1,0, //top right triangle
+            0,1,3  //bottom left triangle
+    };
+    private int vaoID, vboID, eboID;
 
     public LevelEditorScene() {
         System.out.println("Inside the level editing scene");
@@ -45,7 +63,6 @@ public class LevelEditorScene extends Scene {
         //Read vertex and shader file
         //Initialize openGL and link shader
         //Write vertex array for simple triangle/rectangle
-
         // this is for the vertex shader
         vertexID = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexID, vertexShaderSource);
@@ -91,20 +108,80 @@ public class LevelEditorScene extends Scene {
         } else {
             System.out.println("Shaders linked successfully.");
         }
+        //Our VAO, VBO and EBO buffer Objects.
+        //ved ikke om det her er rigtigt
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        //A float buffer of vertices
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        //VBO, the vertex buffer and upload
+        vboID =glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER,vertexBuffer, GL_STATIC_DRAW);
+
+
+        //The indices and upload
+        IntBuffer elementBuffer =BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID =glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,elementBuffer, GL_STATIC_DRAW);
+
+        //Pointers to the vertex attributes
+        int positionsSize =3;
+        int colorSize =4;
+        int floatSizeBytes=4;                        //a size of a float is 4 bytes
+        int vertexSizeBytes=(positionsSize+colorSize)*floatSizeBytes;
+
+        glVertexAttribPointer(0,positionsSize,GL_FLOAT,false,vertexSizeBytes,0);
+        glEnableVertexAttribArray(0);
+
+
+        glVertexAttribPointer(1,colorSize,GL_FLOAT,false,vertexSizeBytes,positionsSize*floatSizeBytes);
+        glEnableVertexAttribArray(1);
+
+
+
     }
     @Override
     public void update(float dt) {
-        if (!changingScene && KeyboardHandler.isKeyPressed(KeyEvent.VK_SPACE)) { // Key to change scene
-            changingScene = true;
-            System.out.println("Changing scene");
-        }
+        //bind shade Program
+        glUseProgram(shaderProgram);
+        //bind the vao that we are using
+        glBindVertexArray(vaoID);
 
-        if (changingScene && timeToChangeScene > 0) {
-            timeToChangeScene -= dt;
-            // Do stuff
+        //enable the vertex attribute pointers
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
 
-        } else if (changingScene) {
-            GameEngineManager.changeScene("GameScene"); // Problem making it static
-        }
+        glDrawElements(GL_TRIANGLES,elementArray.length,GL_UNSIGNED_INT,0);
+
+        //unbind everything
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+
+        glUseProgram(0);
+
+        // if (!changingScene && KeyboardHandler.isKeyPressed(KeyEvent.VK_SPACE)) { // Key to change scene
+        //            changingScene = true;
+        //            System.out.println("Changing scene");
+        //        }
+        //
+        //        if (changingScene && timeToChangeScene > 0) {
+        //            timeToChangeScene -= dt;
+        //            // Do stuff
+        //
+        //        } else if (changingScene) {
+        //            GameEngineManager.changeScene("GameScene"); // Problem making it static
+        //        }
+        //    }
+
+
     }
 }
