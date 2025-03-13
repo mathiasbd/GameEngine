c8d42af4b1cd48d90a6efe060a7fb543571ea427
AhmedAbikar <148544113+AhmedAbikar@users.noreply.github.com>
2025-03-13 15:25:28 +0100

    updated gradle so it supports mac

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,46 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
-
 val lwjglVersion = "3.3.6"
 val jomlVersion = "1.10.7"
-val lwjglNatives = "natives-windows"
-
+val lwjglNatives = listOf(
+    "natives-windows",
+    "natives-macos",
+    "natives-macos-arm64"
+)
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
-
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.joml", "joml", jomlVersion)
     implementation("org.lwjgl", "lwjgl")
     implementation("org.lwjgl", "lwjgl-glfw")
     implementation("org.lwjgl", "lwjgl-openal")
     implementation("org.lwjgl", "lwjgl-opengl")
-    implementation("org.lwjgl", "lwjgl-stb")
-    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
-    implementation("org.joml", "joml", jomlVersion)
+
+    lwjglNatives.forEach { native ->
+        runtimeOnly("org.lwjgl", "lwjgl", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = native)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+application {
+    mainClass.set("org.example.Main")
+}
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("-XstartOnFirstThread")
 }
