92695ece859ec1aa973d53735fd19d9fb86690ba
AhmedAbikar <148544113+AhmedAbikar@users.noreply.github.com>
2025-04-30 10:35:45 +0200

    restoring changes

---
+++ src/main/java/physics/forces/ForceRegistration.java
@@ -0,0 +1,21 @@
+package physics.forces;
+import physics.rigidbody.Rigidbody2D;
+
+public class ForceRegistration {
+    public ForceGenerator fg;
+    public Rigidbody2D rb;
+
+    public ForceRegistration(ForceGenerator fg, Rigidbody2D rb) {
+        this.fg = fg;
+        this.rb = rb;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (other.getClass() != ForceRegistration.class) return false;
+
+        ForceRegistration fr = (ForceRegistration)other;
+        return fr.rb == this.rb && fr.fg == this.fg;
+    }
+}
