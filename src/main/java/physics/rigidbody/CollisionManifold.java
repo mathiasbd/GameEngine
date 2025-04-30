1435b32d5421e61039c8cb4735fc5817d6b87055
s235257 <Gabi04@live.dk>
2025-04-30 10:48:49 +0200

    collision manifolds for circle collision

---
+++ src/main/java/physics/rigidbody/CollisionManifold.java
@@ -0,0 +1,44 @@
+package physics.rigidbody;
+
+import org.joml.Vector2f;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollisionManifold {
+    private Vector2f normal;
+    private List<Vector2f> contactPoints;
+    private float penetrationDepth;
+
+    private boolean isColliding;
+
+    public CollisionManifold() {
+        this.normal = new Vector2f();
+        this.penetrationDepth = 0.0f;
+        this.contactPoints = new ArrayList<>();
+        isColliding = false;
+    }
+
+    public CollisionManifold(Vector2f normal, float penetrationDepth) {
+        this.normal = normal;
+        this.contactPoints = new ArrayList<>();
+        this.penetrationDepth = penetrationDepth;
+        isColliding = true;
+    }
+
+    public void addContactPoint(Vector2f contactPoint) {
+        contactPoints.add(contactPoint);
+    }
+
+    public Vector2f getNormal() {
+        return normal;
+    }
+
+    public List<Vector2f> getContactPoints() {
+        return contactPoints;
+    }
+
+    public float getPenetrationDepth() {
+        return penetrationDepth;
+    }
+}
---
+++ src/main/java/physics/rigidbody/Collisions.java
@@ -0,0 +1,32 @@
+package physics.rigidbody;
+
+import org.joml.Vector2f;
+import physics.primitives.Circle;
+
+import java.util.List;
+
+public class Collisions {
+    public static CollisionManifold findCollisionFeatures(Circle a, Circle b) {
+        CollisionManifold manifold = new CollisionManifold();
+
+        float radiusSum = a.getRadius() + b.getRadius();
+        Vector2f distance = new Vector2f(b.getCenter()).sub(a.getCenter());
+
+        if (distance.lengthSquared() > radiusSum * radiusSum) {
+            return manifold; // No collision
+        }
+
+        float penetrationDepth = Math.abs(distance.length() - radiusSum);
+        float halfPenetrationDepth = penetrationDepth * 0.5f;
+        // This logic is used to get a similar to realistic collision
+        // We might change it to be more realistic by using mass to apply accurate force on each object
+
+        Vector2f normal = new Vector2f(distance).normalize();
+        float distanceToA = a.getRadius() - halfPenetrationDepth;
+        Vector2f contactPointA = new Vector2f(a.getCenter()).add(new Vector2f(normal).mul(distanceToA));
+
+        manifold = new CollisionManifold(normal, penetrationDepth);
+        manifold.addContactPoint(contactPointA);
+        return manifold;
+    }
+}
